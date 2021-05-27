cf1d3efcbf2a4dfdb096a116d7f412303737a301
Sunny-Baishnab <72726626+Sunny-Baishnab@users.noreply.github.com>
2021-05-27 17:22:32 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import PickImage from './screens/Camera'
+
+export default class App extends React.Component {
+  render(){
+    return(
+      <PickImage/>
+    )
+  }
+}
+
